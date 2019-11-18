5a2dc02b902f122bd859fbf80dd872f36ed70352
Guillaume Queste <guillaume.queste@laposte.net>
2019-11-18 17:42:33 +0100

    curl_get_file_contents()

--- index.php
+++ index.php
@@ -1,31 +1,42 @@
 <?php
+function curl_get_file_contents($URL){
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($c, CURLOPT_URL, $URL);
+    $contents = curl_exec($c);
+    curl_close($c);
+
+    if ($contents) return $contents;
+    else return FALSE;
+}
+
 $definitions = 'https://api.dicolink.com/v1/mot/'.$_POST["search"].'/definitions?limite=200&api_key=F49u6WGSR1N4WV_w4Tb9vva_ENf2gc1I';
-$data_definitions = file_get_contents($definitions);
+$data_definitions = curl_get_file_contents($definitions);
 $data_definitions = json_decode($data_definitions);
 $def = $data_definitions;
 
 $citations = 'https://api.dicolink.com/v1/mot/'.$_POST["search"].'/citations?limite=5&api_key=F49u6WGSR1N4WV_w4Tb9vva_ENf2gc1I';
-$data_citations = file_get_contents($citations);
+$data_citations = curl_get_file_contents($citations);
 $data_citations = json_decode($data_citations);
 $cit = $data_citations;
 
 $expressions = 'https://api.dicolink.com/v1/mot/'.$_POST["search"].'/expressions?limite=5&api_key=F49u6WGSR1N4WV_w4Tb9vva_ENf2gc1I';
-$data_expressions = file_get_contents($expressions);
+$data_expressions = curl_get_file_contents($expressions);
 $data_expressions = json_decode($data_expressions);
 $exp = $data_expressions;
 
 $synonymes = 'https://api.dicolink.com/v1/mot/'.$_POST["search"].'/synonymes?limite=5&api_key=F49u6WGSR1N4WV_w4Tb9vva_ENf2gc1I';
-$data_synonymes = file_get_contents($synonymes);
+$data_synonymes = curl_get_file_contents($synonymes);
 $data_synonymes = json_decode($data_synonymes);
 $syn = $data_synonymes;
 
 $antonymes = 'https://api.dicolink.com/v1/mot/'.$_POST["search"].'/antonymes?limite=5&api_key=F49u6WGSR1N4WV_w4Tb9vva_ENf2gc1I';
-$data_antonymes = file_get_contents($antonymes);
+$data_antonymes = curl_get_file_contents($antonymes);
 $data_antonymes = json_decode($data_antonymes);
 $ant = $data_antonymes;
 
 $scorescrabble = 'https://api.dicolink.com/v1/mot/'.$_POST["search"].'/scorescrabble?limite=5&api_key=F49u6WGSR1N4WV_w4Tb9vva_ENf2gc1I';
-$data_scorescrabble = file_get_contents($scorescrabble);
+$data_scorescrabble = curl_get_file_contents($scorescrabble);
 $data_scorescrabble = json_decode($data_scorescrabble);
 $ssc = $data_scorescrabble;
 ?>
@@ -46,7 +57,7 @@ $ssc = $data_scorescrabble;
     <div class="form">
         <form action="" method="post">
             <input type="text" name="search" placeholder="Rechercher un mot" class="input_search" />
-            <input type="submit" value="OK" name="btn_search"><?php if (!empty($_POST['btn_search'])): ?><div class="search">cheval</div><?php endif ?>
+            <input type="submit" value="OK" name="btn_search"><?php if (!empty($_POST['btn_search'])): ?><div class="search"><?= $_POST['search'] ?></div><?php endif ?>
         </form>
     </div>
 
